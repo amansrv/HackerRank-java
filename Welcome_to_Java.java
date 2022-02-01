dcbc66852500b58ddccbef7b95de3511631823cd
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-02-01 22:03:20 +0530

    Create Welcome_to_Java.java

---
+++ Welcome_to_Java.java
@@ -0,0 +1,16 @@
+//   Input Format
+//   here is no input for this challenge.
+
+//   Output Format
+//   You must print two lines of output:
+//   Print Hello, World. on the first line.
+//   Print Hello, Java. on the second line.
+  
+  
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
